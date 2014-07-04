0b67b305b551c2b80f9dbd21c192ab0a3b10be85
Matt Witherow <hi@mattwitherow>
2014-07-04 16:59:38 +1000

    added survey adpater

--- src/com/android/smap/adapters/SurveyAdapter.java
+++ src/com/android/smap/adapters/SurveyAdapter.java
@@ -1,15 +1,20 @@
 package com.android.smap.adapters;
 
+import java.util.List;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.smap.R;
+import com.android.smap.api.models.Survey;
 import com.android.smap.ui.VelocAdapter;
 import com.android.smap.ui.ViewQuery;
 
 public class SurveyAdapter extends VelocAdapter {
+
+	List<Survey>	mModel;
 
 	public SurveyAdapter(Context context) {
 		super(context);
@@ -19,26 +24,27 @@ public class SurveyAdapter extends VelocAdapter {
 	public View newView(LayoutInflater inflator, int position, ViewGroup parent) {
 
 		return inflator.inflate(R.layout.item_survey, parent);
-
 	}
 
 	@Override
 	public void bindView(Context context, View view, ViewQuery query,
 			int position) {
-		// TODO Auto-generated method stub
+
+		query.find(R.id.txt_survey).text(mModel.get((position)).name);
+		query.find(R.id.txt_survey).text(mModel.get((position)).completed);
+		query.find(R.id.txt_survey).text(mModel.get((position)).members);
+		query.find(R.id.txt_survey).text(mModel.get((position)).partial);
 
 	}
 
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return mModel.size();
 	}
 
 	@Override
 	public Object getItem(int position) {
-		// TODO Auto-generated method stub
-		return null;
+		return mModel.get(position);
 	}
 
 }
