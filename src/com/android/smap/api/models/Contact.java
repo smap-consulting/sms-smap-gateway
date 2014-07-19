17d2d79d64fadda56ac4de9b4b2afb48e715ae5d
Chris Atkins <christopherlionelatkins@gmail.com>
2014-07-19 16:01:54 +1000

    Contacts are now persistable

--- src/com/android/smap/api/models/Contact.java
+++ src/com/android/smap/api/models/Contact.java
@@ -1,8 +1,16 @@
 package com.android.smap.api.models;
 
-public class Contact {
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
 
+@Table(name = "contacts")
+public class Contact extends Model {
+
+	@Column
 	public String	number;
+
+	@Column
 	public String	name;
 
 }
