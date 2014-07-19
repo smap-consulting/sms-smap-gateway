cb906c9508e1d07b1e4527c9ebb2f5e8a7278516
Chris Atkins <christopherlionelatkins@gmail.com>
2014-07-19 16:08:06 +1000

    added some finders to the data models

--- src/com/android/smap/api/models/Contact.java
+++ src/com/android/smap/api/models/Contact.java
@@ -1,16 +1,27 @@
 package com.android.smap.api.models;
 
+import java.util.List;
+
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
 
 @Table(name = "contacts")
 public class Contact extends Model {
 
 	@Column
-	public String	number;
+	public String number;
 
 	@Column
-	public String	name;
+	public String name;
+	
+	public static Contact findById(Long id) {
+		return Model.load(Contact.class, id);
+	}
+	
+	public static List<Contact> findAll() {
+		return new Select().from(Contact.class).execute(); 
+	}
 
 }
--- src/com/android/smap/api/models/Survey.java
+++ src/com/android/smap/api/models/Survey.java
@@ -1,12 +1,27 @@
 package com.android.smap.api.models;
 
-public class Survey {
+import java.util.List;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
+@Table(name = "surveys")
+public class Survey extends Model {
+	
+	@Column
 	public String name;
+	
 	public int members;
 	public int completed;
 	public int partial;
-	public int id;
 	
-	//etc
+	public static Survey findById(Long id) {
+		return Model.load(Survey.class, id);
+	}
+	
+	public static List<Survey> findAll() {
+		return new Select().from(Survey.class).execute();
+	}
 }
