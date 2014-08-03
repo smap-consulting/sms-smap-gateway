b1a9cb5070b6c12171f07dade3412e352d82fb48
Chris Atkins <christopherlionelatkins@gmail.com>
2014-08-03 16:31:45 +1000

    temporarily seeding some surveys if the db is empty

--- src/com/android/smap/api/models/SqliteDataManager.java
+++ src/com/android/smap/api/models/SqliteDataManager.java
@@ -2,27 +2,40 @@ package com.android.smap.api.models;
 
 import java.util.List;
 
+import com.activeandroid.ActiveAndroid;
 import com.android.smap.di.DataManager;
 
+
 public class SqliteDataManager implements DataManager {
+	
+	public SqliteDataManager() {
+		
+		if (getSurveys().isEmpty()) {
+			seedDevData();
+		}
+	}
 
 	@Override
 	public List<Survey> getSurveys() {
+
 		return Survey.findAll();
 	}
 
 	@Override
 	public Survey getDetailsForSurvey(int id) {
+
 		return Survey.findById((long) id);
 	}
 
 	@Override
 	public List<Contact> getContacts() {
+
 		return Contact.findAll();
 	}
 
 	@Override
 	public void putContacts(List<Contact> contacts) {
+		
 		for(Contact contact : contacts) {
 			contact.save();
 		}
@@ -35,4 +48,17 @@ public class SqliteDataManager implements DataManager {
 		
 	}
 
+	private void seedDevData() {
+		
+		ActiveAndroid.beginTransaction();
+		try {
+			for(int n = 1; n <= 10; n++) {
+				Survey survey = new Survey("Survey " + n);
+				survey.save();
+			}
+			ActiveAndroid.setTransactionSuccessful();
+		} finally {
+			ActiveAndroid.endTransaction();
+		}
+	}
 }
