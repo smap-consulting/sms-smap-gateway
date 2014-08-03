ba75532305d317eb626872fb6e28d4bffa167c4b
Chris Atkins <christopherlionelatkins@gmail.com>
2014-08-03 17:23:46 +1000

    helper to lookup SurveyContacts by contact and survey id

--- src/com/android/smap/api/models/SurveyContact.java
+++ src/com/android/smap/api/models/SurveyContact.java
@@ -4,17 +4,27 @@ import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Column.ForeignKeyAction;
 import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
 
 @Table(name = "surveys_contacts")
 public class SurveyContact extends Model {
 	
-	@Column(name = "survey_id", onDelete=ForeignKeyAction.CASCADE)
+	@Column(name = "survey_id", onDelete = ForeignKeyAction.CASCADE)
 	public Survey survey;
 	
-	@Column(name = "contact_id", onDelete=ForeignKeyAction.CASCADE)
+	@Column(name = "contact_id", onDelete = ForeignKeyAction.CASCADE)
 	public Contact contact;
 	
 	public int		answers;
 	public int		total;
 	public String	updatedAt;
+	
+	public static SurveyContact findBySurveyAndContactIds(long surveyId, long contactId) {
+
+		return new Select()
+			.from(SurveyContact.class)
+			.where("survey_id = ?", surveyId)
+			.where("contact_id = ?", contactId)
+			.executeSingle();
+	}
 }
